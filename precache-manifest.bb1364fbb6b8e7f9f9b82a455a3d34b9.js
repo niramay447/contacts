284218080db978848187083aacb085ac263d9868
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-26 12:36:13 +0530

    Updates

---
+++ precache-manifest.bb1364fbb6b8e7f9f9b82a455a3d34b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a908628bd2487dd84cb627e6197691de",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/contacts/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "1ecb3705ec6d94108bc9",
+    "url": "/contacts/static/js/main.96fb3096.chunk.js"
+  },
+  {
+    "revision": "61725dbfcef51c0710e1",
+    "url": "/contacts/static/js/runtime-main.99e35f6c.js"
+  }
+]);
